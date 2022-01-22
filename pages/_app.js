079b15345c3505e83cdaca27feaed0d5ae461e5e
vnshgrg <ghale.avinash@gmail.com>
2022-01-22 18:04:41 +0900

    added content-security-policy

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
+import Head from 'next/head';
 import '../styles/tailwind.css';
 const qsRemit = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <meta
+                    http-equiv="Content-Security-Policy"
+                    content="upgrade-insecure-requests"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 };
 
 export default qsRemit;
