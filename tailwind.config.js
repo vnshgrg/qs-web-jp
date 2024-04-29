a0a828eabbff782b45e05dfe16ba4997d85fc6e2
Avinash <ghale.avinash@gmail.com>
2024-04-29 22:15:34 +0900

    remove unused tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,19 +15,10 @@ module.exports = {
       },
     },
   },
-  //   purge: [
-  //     "./pages/**/*.jsx",
-  //     "./pages/**/*.js",
-  //     "./app/components/**/*.jsx",
-  //     "./app/components/**/*.js",
-  //     "./app/layout/**/*.jsx",
-  //     "./app/layout/**/*.js",
-  //   ],
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/aspect-ratio"),
-    // require("@tailwindcss/line-clamp"),
     require("tw-elements/dist/plugin"),
   ],
 };
